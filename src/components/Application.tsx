331316204239cf4c60dfa5617430b77d710724fb
kmyasnikov <K.Myasnikov@ase-ec.ru>
2023-10-29 14:36:30 +0300

    first boilerplate

---
+++ src/components/Application.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Application.scss';
+
+const Application: React.FC = () => {
+  return (
+    <div className='content'>
+      test
+    </div>
+  );
+};
+
+export default Application;
